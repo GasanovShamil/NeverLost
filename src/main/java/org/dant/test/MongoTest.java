6e8a812b5ed54e862563618aa2f24146a37a52eb
GasanovShamil <gasanov.sh@gmail.com>
2016-05-23 19:32:57 +0200

    script  initialisation BD

--- src/main/java/org/dant/test/MongoTest.java
+++ src/main/java/org/dant/test/MongoTest.java
@@ -19,6 +19,7 @@ public class MongoTest {
 			// Now connect to your databases
 			MongoDatabase db = mongoClient.getDatabase("neverlost");
 
+			db.getCollection("users").drop();
 			db.getCollection("users")
 					.insertOne(new Document("email", "shamil@mail.com").append("username", "shamilevich")
 							.append("password", "shamil").append("lon", 0.0).append("lat", 0.0)
@@ -30,6 +31,14 @@ public class MongoTest {
 			db.getCollection("users")
 					.insertOne(new Document("email", "milan@mail.com").append("username", "bubachvaba")
 					.append("password", "milan").append("lon", 0.0).append("lat", 0.0)
+					.append("friends", new ArrayList<Document>()));
+			db.getCollection("users")
+					.insertOne(new Document("email", "ibra@mail.com").append("username", "brambaba")
+							.append("password", "ibra").append("lon", 0.0).append("lat", 0.0)
+							.append("friends", new ArrayList<Document>()));
+			db.getCollection("users")
+			.insertOne(new Document("email", "khaled@mail.com").append("username", "bouboubond")
+					.append("password", "khaled").append("lon", 0.0).append("lat", 0.0)
 					.append("friends", new ArrayList<Document>()));
 			mongoClient.close();
 		} catch (Exception e) {
